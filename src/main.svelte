a414dd30f6024a9d1415e0c486c5ed63146b1a8a
Felix <81219766+idkfelix@users.noreply.github.com>
2024-03-16 11:54:48 +1100

    Update main.svelte

--- src/main.svelte
+++ src/main.svelte
@@ -1,34 +1,34 @@
 <script>
-  import './lib/types/calendar'
   import CompassClient from './lib/compass'
+  import './lib/types/calendar'
 
   export let sessionId
   let date = new Date().toISOString().slice(0,10)
-  let client
+  const client = await CompassClient('mullauna-vic.compass.education','ASP.NET_SessionId='+sessionId)
 
   /** @type {Promise<CalendarResponse>} */
-  let res
-  $: {res = fetchData(date)}
+  let data
 
   async function incrementDate(offset) {
     const newDate = new Date(date)
     newDate.setDate(newDate.getDate() + offset)
     date = newDate.toISOString().slice(0,10)
-    res = fetchData(date)
+    data = fetchData(date)
   }
 
   async function fetchData(date) {
-    client = await CompassClient('mullauna-vic.compass.education','ASP.NET_SessionId='+sessionId)
     let periods = await client.Calendar.getCalendarEventsByUser(client.userId, date, date)
     return periods.sort((a,b) => a.period - b.period)
   }
+
+  $: {data = fetchData(date)}
 </script>
 
 <div style="max-width: 300px;max-height:75px">
   <h2 class="HyperMD-header HyperMD-header-2 cm-line">{date}</h2>
   <button on:click={()=>{incrementDate(-1)}}>Previous</button>
   <button on:click={()=>{incrementDate(+1)}}>Next</button>
-  {#await res}
+  {#await data}
     <h3>Loading...</h3>
   {:then periods} 
     {#each periods as period}
